a31fcdb889a1b7bb86712fd700482901f672c391
xu <1669228230>
2022-09-01 12:09:11 +0800

    继承Junit单元测试与Logback日志

---
+++ src/main/java/com/imooc/reader/mapper/TestMapper.java
@@ -0,0 +1,14 @@
+package com.imooc.reader.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName: TestMapper
+ * @Description: 测试接口
+ * @author: XU
+ * @date: 2022/9/1 11:31
+ **/
+@Repository
+public interface TestMapper {
+    public void insertSample();
+}
